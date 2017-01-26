e9d899f4ea4e2bec1af544df05765db8c8ef5d38
SmnThms <simon.thomas8@gmail.com>
2017-01-27 00:39:31 +0100

    Add files via upload

--- generateur_de_son.py
+++ generateur_de_son.py
@@ -25,12 +25,13 @@ class Son:
     """  
     def __init__(self, debut, hauteur, timbre, enveloppe, duree=3):
         self.debut = debut*taux
-        self.duree = duree*taux + 1 # Pour la fréquence 0 de la TF
-        self.positives = np.concatenate((R[timbre].positives[1000-np.floor(R[hauteur]):],np.zeros(self.duree))) # Fréquences positives du timbre,transposées
-        self.negatives = np.concatenate((R[timbre].negatives[1000-np.floor(R[hauteur]):],np.zeros(self.duree))) # Fréquences négatives du timbre,transposées
-        self.spectre = np.concatenate((np.array([0]),self.positives[:self.duree//2],self.negatives[:self.duree//2])) # Assemblage du spectre
+        self.duree = duree*taux
+        self.spectre = np.zeros(self.duree//2+1)
+        timbre_transpose = R[timbre].freq[1000-np.floor(R[hauteur]):]
+        self.spectre[:duree*len(timbre_transpose):duree] = timbre_transpose
         self.enveloppe = Enveloppe(enveloppe,self.duree).profil # Evolution de l'intensité au cours du son
-        self.signal = np.fft.ifft(self.spectre)*self.enveloppe
+        self.signal = np.fft.irfft(self.spectre)*self.enveloppe
+        self.signal *= 0.2/np.max(self.signal)
         
 class Timbre:
     """
@@ -39,31 +40,28 @@ class Timbre:
     def __init__(self,parametre=0.01):
         # Pour créer un timbre à partir de l'enregistrement sonore d'une note
         if type(parametre) is str: 
-            SR, extrait_sonore = scipy.io.wavfile.read(parametre+'.wav')            
-            spectre_extrait = np.fft.fft(extrait_sonore)
+            SR, extrait_sonore = scipy.io.wavfile.read(parametre+'.wav')  
+            spectre_extrait = np.fft.rfft(extrait_sonore[:,0])
+            self.test = extrait_sonore
             if np.argmax(spectre_extrait)<1000:
-                self.positives = np.concatenate((np.zeros(1000-np.argmax(spectre_extrait)),spectre_extrait[:9000+np.argmax(spectre_extrait)]))
+                self.freq = np.concatenate((np.zeros(1000-np.argmax(spectre_extrait)),spectre_extrait[:9000+np.argmax(spectre_extrait)]))
             else:
-                self.positives = spectre_extrait[np.argmax(spectre_extrait):10000]
-         # Pour créer un timbre constitué d'un fondamental éventuellement gaussien
+                self.freq = spectre_extrait[:10000]
+        # Pour créer un timbre constitué d'un fondamental éventuellement gaussien
         else:
-            self.positives = np.exp(-(np.arange(10000)-999)**2/(2*parametre**2))
-        self.negatives = self.positives
+            self.freq = np.exp(-(np.arange(10000)-999)**2/(2*parametre**2))
         
     def harmoniques(self,coefs):
         for i,amplitude in enumerate(coefs):
-            self.positives[1000*(i+2)/(i+1)] += amplitude
-            self.negatives[1000*(i+2)/(i+1)] += amplitude
+            self.freq[1000*(i+2)/(i+1)] += amplitude
         return self
             
     def passe_bas(self,frequence_coupure,largeur=200):
-        self.positives *= filtre(frequence_coupure,largeur,10000)[::-1]
-        self.negatives *= filtre(frequence_coupure,largeur,10000)[::-1]
+        self.freq *= filtre(frequence_coupure,largeur,10000)[::-1]
         return self
         
     def passe_haut(self,frequence_coupure,largeur=200):
-        self.positives *= filtre(frequence_coupure,largeur,10000)
-        self.negatives *= filtre(frequence_coupure,largeur,10000)
+        self.freq *= filtre(frequence_coupure,largeur,10000)
         return self
     
 class Enveloppe:
@@ -95,7 +93,6 @@ def jouer(partition):
             if len(signal)<son.debut+son.duree:
                 signal = np.concatenate((signal,np.zeros(son.debut+son.duree-len(signal))))
             signal[son.debut:son.debut+len(son.signal)] += son.signal
-    signal *= 0.2/np.max(signal) # Normalisation du volume
 # La syntaxe qui suit est tirée de davywybiral.blogspot.fr/2010/09/procedural-music-with-pyaudio-and-numpy.html
     p = pyaudio.PyAudio()
     stream = p.open(format=pyaudio.paFloat32, channels=1, rate=44100, output=1)
@@ -112,13 +109,15 @@ def filtre(coupure, largeur, taille):
    # Timbres
 R={'1':Timbre(10).harmoniques((0.5,0.2,0.2,0.1,0.04,0.06))}
 R.update({'2':Timbre(70).harmoniques((0,0,0.2,0.1,0.5,0.6,0.4,0.2,0.1))})
-R.update({'3':Timbre(0.01).harmoniques((0.5,0,0.1,0,0.04,0,0.01,0,0.01)).passe_bas(350)})
+R.update({'3':Timbre()})
+R.update({'4':Timbre().harmoniques((0.1,0.05,0.1,0.05))})
+R.update({'voix':Timbre('1')})
    # Notes
 for i,clef in enumerate(('a','z','e','r','t','y','u','i','o','p')):
     R.update({clef:261.63*2**(i/12)})
+R.update({'q':60,'s':60/(2**(1/12))})
 
-
-
-partition = (Son(0,'a', '1', 'sin'),Son(1,'a','2','lin',4),Son(3,'r','1','sin'),Son(4,'p','3','sin'))
+partition = (Son(0,'e','3','sin',duree=4),Son(2,'i','3','sin'),Son(3,'p','3','sin'),Son(4,'e','3','sin',duree=4),Son(5,'q','4','lin',duree=3),Son(6,'s','4','sin',duree=4),Son(8,'e','voix','lin'),Son(10,'y','voix','lin'))
+#partition = (Son(0,'a', '1', 'sin'),Son(1,'a','2','lin',4),Son(3,'r','1','sin'),Son(4,'p','3','sin'))
 s = jouer(partition)
-plt.plot(s)
+#plt.plot(s)
